47b74202112e01001a776cb59ffe9454a544dc85
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-09 20:58:12 +0100

    fade problem

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -363,6 +363,17 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-24',
       name: 'Aenean Ru Bristique 24',
+      category: 'chair',
+      image: 'src=../../images/bed2.jpeg',
+      price: 30,
+      olderPrice: `$ ${55}`,
+      stars: 2,
+      promo: 'sale',
+      newFurniture: true,
+    },
+    {
+      id: 'aenean-ru-bristique-22',
+      name: 'Aenean Ru Bristique 22',
       category: 'bed',
       image: 'src=../../images/bed3.jpeg',
       price: 30,
@@ -372,6 +383,7 @@ const initialState = {
       newFurniture: true,
     },
   ],
+
   cart: {
     products: [],
   },
