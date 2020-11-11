e68515803c01e8ded882067c2629f2b9b7ba32d3
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-11 21:04:09 +0100

    Add saleContentReducer to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import initialState from './initialState';
 import cartReducer from './cartRedux';
 import categoriesReducer from './categoriesRedux';
 import productsReducer from './productsRedux';
+import saleContentReducer from './saleRedux';
 
 // define reducers
 const reducers = {
   cart: cartReducer,
   categories: categoriesReducer,
   products: productsReducer,
+  saleContent: saleContentReducer,
 };
 
 // add blank reducers for initial state properties without reducers
