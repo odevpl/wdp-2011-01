6903bf0287dd60a4c5116de048e54a4fd7e3fd2a
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-11-11 12:10:09 +0100

    Add RWD for brands component elems and try to add swipe on this elements

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -3,28 +3,37 @@ import styles from './Brands.module.scss';
 import Button from '../../common/Button/Button';
 import PropTypes from 'prop-types';
 
-const Brands = ({ brands }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className={'row align-items-center ' + styles.wrapper}>
-        <Button className={styles.button}>
-          <span className={styles.arrow}> &lt; </span>
-        </Button>
-        <ul className={styles.list}>
-          {brands.map(brand => (
-            <li
-              key={brand.id.replace('brand-', '')}
-              style={{ backgroundImage: `url('${brand.image}')` }}
-            ></li>
-          ))}
-        </ul>
-        <Button className={styles.button}>
-          <span className={styles.arrow}> &gt; </span>
-        </Button>
+class Brands extends React.Component {
+  render() {
+    const { brands } = this.props;
+
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={'row align-items-center ' + styles.wrapper}>
+            <Button className={styles.button}>
+              <span className={styles.arrow}> &lt; </span>
+            </Button>
+            <ul className={styles.list}>
+              {brands.map(brand => {
+                return (
+                  <li
+                    key={brand.id.replace('brand-', '')}
+                    style={{ backgroundImage: `url('${brand.image}')` }}
+                  ></li>
+                );
+              })}
+              ;
+            </ul>
+            <Button className={styles.button}>
+              <span className={styles.arrow}> &gt; </span>
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 Brands.propTypes = {
   map: PropTypes.func,
