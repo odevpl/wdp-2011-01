c70a873609fb5aeb964449baffaf8a1c5c606930
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-11-14 23:13:58 +0100

    Add Feedback functionality

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swipe from 'react-easy-swipe';
 import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,12 +11,26 @@ class Feedback extends React.Component {
     activePage: 0,
   };
 
-  handlePageChange(no) {}
+  handlePageChange(newPage) {
+    this.setState({ activePage: newPage });
+  }
+
+  handleRightSwipe() {
+    if (this.state.activePage >= 1) {
+      this.handlePageChange(this.state.activePage - 1);
+    }
+  }
+
+  handleLeftSwipe(pagesCount) {
+    if (this.state.activePage < pagesCount - 1) {
+      this.handlePageChange(this.state.activePage + 1);
+    }
+  }
 
   render() {
     const { feedback, count } = this.props;
     const { activePage } = this.state;
-    const item = feedback[0];
+    const item = feedback[activePage];
 
     const dots = [];
     for (let i = 0; i < count; i++) {
@@ -32,35 +47,43 @@ class Feedback extends React.Component {
     }
 
     return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.panelBar}>
-            <div className='row no-gutters align-items-end'>
-              <div className={'col-12 ' + styles.heading}>
-                <h3>Client feedback</h3>
-              </div>
-              <div className={'col-auto ' + styles.dots}>
-                <ul>{dots}</ul>
-              </div>
-            </div>
-            <div key={item.id} className={'row justify-content-center ' + styles.item}>
-              <div className='row justify-content-center'>
-                <div className={'col-2 ' + styles.subitem}>
-                  <FontAwesomeIcon className={styles.icon} icon={faQuoteRight} />
+      <Swipe
+        onSwipeLeft={() => this.handleLeftSwipe(count)}
+        onSwipeRight={() => this.handleRightSwipe()}
+      >
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={styles.panelBar}>
+              <div className='row no-gutters align-items-end'>
+                <div className={'col-12 ' + styles.heading}>
+                  <h3>Client feedback</h3>
                 </div>
-                <div className={'col-12 ' + styles.subitem}>{item.content}</div>
+                <div className={'col-auto ' + styles.dots}>
+                  <ul>{dots}</ul>
+                </div>
               </div>
-              <div className={'row subitem justify-content-center ' + styles.subitem}>
-                <img className='col-1' src={item.photo} />
-                <div className='col-2'>
-                  <h3>{item.name}</h3>
-                  <h4>{item.position}</h4>
+              <div
+                key={item.id}
+                className={'row justify-content-center ' + styles.item}
+              >
+                <div className='row justify-content-center'>
+                  <div className={'col-2 ' + styles.subitem}>
+                    <FontAwesomeIcon className={styles.icon} icon={faQuoteRight} />
+                  </div>
+                  <div className={'col-12 ' + styles.subitem}>{item.content}</div>
+                </div>
+                <div className={'row subitem justify-content-center ' + styles.subitem}>
+                  <img className='col-1' src={item.photo} />
+                  <div className='col-2'>
+                    <h3>{item.name}</h3>
+                    <h4>{item.position}</h4>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Swipe>
     );
   }
 }
