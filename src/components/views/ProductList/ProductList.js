c3e1c7bdbaefec60b874cac121efb6fd04f78253
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-15 23:25:56 +0100

    Add Banner.js and Banner.scss

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
+import Banner from '../../features/Banner/Banner';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => (
+  <div>
+    <div className={styles.root}>This is ProductList</div>
+    <Banner></Banner>
+  </div>
+);
 
 // ProductList.propTypes = {};
 
