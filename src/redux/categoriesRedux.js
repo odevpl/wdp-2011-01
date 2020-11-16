f62edac441e5669ee83d62d7e60417ecfa52fe63
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-16 20:08:35 +0100

    remove unnecessary code

--- src/redux/categoriesRedux.js
+++ src/redux/categoriesRedux.js
@@ -19,18 +19,6 @@ export const changeFade = payload => ({ payload, type: CHANGE_FADE });
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
-    case CHANGE_FADE: {
-      const newStatePart = statePart.map(category => {
-        category.fade = false;
-        if (category.id === action.payload) {
-          category.fade = !category.fade;
-          return category;    
-        } else {
-          return category;
-        }
-      });
-      return newStatePart;
-    }
     default:
       return statePart;
   }
