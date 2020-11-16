b6088439ca0532ecfa4b6dcec85a51ee2983dee2
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-16 22:07:30 +0100

    Change Sale component design

--- src/components/common/Sale/Sale.js
+++ src/components/common/Sale/Sale.js
@@ -9,19 +9,29 @@ const Sale = ({ saleContent }) => (
         <div className={styles.firstPhoto}>
           <img src={saleContent.sofa} />
           <span></span>
-          <h2>{saleContent.content1}</h2>
+          <h2>{saleContent.content1a}</h2>
+          <p>{saleContent.content1b}</p>
           <h1>{saleContent.bargain}</h1>
         </div>
         <div className={styles.secondPhoto}>
-          <img src={saleContent.yellowChair} />
-          <h4>{saleContent.content2}</h4>
-          <h3>{saleContent.price}</h3>
-          <img src={saleContent.goldenChair} />
+          <img className={styles.doggo} src={saleContent.yellowChair} />
+          <div className={styles.secondWrapper}>
+            <h4 className={styles.contentBold}>{saleContent.content2a}</h4>
+            <h4 className={styles.contentNormal}>{saleContent.content2b}</h4>
+            <h6 className={styles.contentSmall}>{saleContent.content2c}</h6>
+            <h3>{saleContent.price}</h3>
+          </div>
+          <img className={styles.chair} src={saleContent.goldenChair} />
         </div>
         <div className={styles.thirdPhoto}>
           <img src={saleContent.bed} />
-          <h3>{saleContent.content3}</h3>
-          <h4>{saleContent.info}</h4>
+          <div className={styles.thirdWrapper}>
+            <div>
+              <h4 className={styles.thirdBold}>{saleContent.content3a}</h4>
+              <h4 className={styles.thirdNormal}>{saleContent.content3b}</h4>
+            </div>
+            <h5>{saleContent.info}</h5>
+          </div>
         </div>
       </div>
     </div>
