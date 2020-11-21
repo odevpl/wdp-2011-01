177dd37ac2cd3d53cdf5d9f0547937eba3a1fa5e
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-11-21 13:49:28 +0100

    in Gallery.js add slider, take data from initialState

--- src/components/common/Gallery/Gallery.js
+++ src/components/common/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Gallery.module.scss';
+import Button from '../../common/Button/Button';
 
 class Gallery extends React.Component {
   state = {
@@ -19,53 +20,63 @@ class Gallery extends React.Component {
       <div className={styles.root}>
         <div className='row'>
           <div className='col-6'>
-            <div className={'col-auto ' + styles.heading}>
-              <h3>Furniture Gallery</h3>
+            <div className='row no-gutters align-items-end'>
+              <div className={'col-auto ' + styles.heading}>
+                <h3>Furniture Gallery</h3>
+              </div>
             </div>
-            <div className={styles.line}></div>
-            
-            <div className='container'>
+            <div className={'container'}>
               <nav>
-                <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                  <a className="nav-item nav-link active" id="nav-feature-tab" data-toggle="tab" href="#nav-freature" role="tab" aria-controls="nav-freature" aria-selected="true">FREATURE</a>
-                  <a className="nav-item nav-link " id="nav-topseller-tab" data-toggle="tab" href="#nav-topseller" role="tab" aria-controls="nav-topseller" aria-selected="true">TOP SELLER</a>
-                  <a className="nav-item nav-link " id="nav-saleoff-tab" data-toggle="tab" href="#nav-saleoff" role="tab" aria-controls="nav-saleoff" aria-selected="true">SALE OFF</a>
-                  <a className="nav-item nav-link " id="nav-topratted-tab" data-toggle="tab" href="#nav-topratted" role="tab" aria-controls="nav-topratted" aria-selected="true">TOP RATTED</a>
+                <div className='nav nav-tabs' id='nav-tab' role='tablist'>
+                  <a className='nav-item nav-link active' id='nav-feature-tab' data-toggle='tab' href="#nav-freature" role='tab' aria-controls='nav-freature' aria-selected='true'>FREATURE</a>
+                  <a className='nav-item nav-link ' id='nav-topseller-tab' data-toggle='tab' href="#nav-topseller" role='tab' aria-controls='nav-topseller' aria-selected='true'>TOP SELLER</a>
+                  <a className='nav-item nav-link ' id='nav-saleoff-tab' data-toggle='tab' href="#nav-saleoff" role='tab' aria-controls='nav-saleoff' aria-selected='true'>SALE OFF</a>
+                  <a className='nav-item nav-link ' id='nav-topratted-tab' data-toggle='tab' href="#nav-topratted" role='tab' aria-controls='nav-topratted' aria-selected='true'>TOP RATTED</a>
                 </div>
               </nav>
-              <div className="tab-content" id="nav-tabContent">
-                <div className="tab-pane fade show active" id="nav-freature" role="tabpanel"aria-labelledby="nav-freature-tab">
+              <div className='tab-content' id='nav-tabContent'>
+                <div className='tab-pane fade show active' id='nav-freature' role='tabpanel' aria-labelledby='nav-freature-tab'>
                   {/*feature gallery*/}
-                  
-                  <img id="expandedImg" src='../../images/bed7.jpeg' />
-                    
-                  <div className='row'>
-                    <div className='col'>
-                      <img src='../../images/chair1.jpeg' />
+                  <div className='col-12'>
+                    <img id="expandedImg" src='../../images/bed7.jpeg' />
+                  </div>
+                  <div className={'row align-items-center ' + styles.wrapper}>
+                    <Button className={styles.button} onClick={() => this.handleLeftSlide(this.state.margin)}
+                    >
+                      <span className={styles.arrow}> &lt; </span>
+                    </Button>
+                    <div className={styles.galleryParent}>
+                      <div className={styles.galleryChild} style={{ marginLeft: this.state.margin }}
+                      >
+                        {gallery.map(feature => {
+                          return (
+                            <div className={styles.image} key={feature.id.replace('feature-', '')}>
+                              <img src={feature.image} alt={feature.id} />
+                            </div>
+                          );
+                        })}
+                      </div>
                     </div>
-                    <div className='col'>
-                      <img src='../../images/table1.jpeg' />
-                    </div>
-                    <div className='col'>
-                      <img src='../../images/bed2.jpeg' />
-                    </div>
+                    <Button className={styles.button + ' ' + styles.buttonTwo} onClick={() => this.handleRightSlide(this.state.margin)}
+                    >
+                      <span className={styles.arrow}> &gt; </span>
+                    </Button>
                   </div>
                 </div>
-              </div>
-              <div className="tab-pane fade " id="nav-topseller" role="tabpanel" aria-labelledby="nav-topseller-tab">
-                {/*top seller gallery*/}
-              </div>
-              <div className="tab-pane fade " id="nav-saleoff" role="tabpanel" aria-labelledby="nav-saleoff-tab">
-                {/*sale off gallery*/}
-              </div>
-              <div className="tab-pane fade " id="nav-saleoff" role="tabpanel" aria-labelledby='nav-topratted-tab'>
-                {/*top ratted gallery</div>*/}
+                <div className="tab-pane fade " id="nav-topseller" role="tabpanel" aria-labelledby="nav-topseller-tab">
+                  {/*top seller gallery*/}
+                </div>
+                <div className="tab-pane fade " id="nav-saleoff" role="tabpanel" aria-labelledby="nav-saleoff-tab">
+                  {/*sale off gallery*/}
+                </div>
+                <div className="tab-pane fade " id="nav-saleoff" role="tabpanel" aria-labelledby='nav-topratted-tab'>
+                  {/*top ratted gallery</div>*/}
+                </div>
               </div>
             </div>
           </div>
-            
           <div className='col-6'>
-            <img src='../../images/bed1.jpeg' />
+          <img src='../../images/bed1.jpeg' />
             <h2>
               <span>FROM </span> $50.80
             </h2>
@@ -74,7 +85,7 @@ class Gallery extends React.Component {
               <a href=''>SHOP NOW</a>
             </h4>
           </div>
-        </div>
+        </div> 
       </div>
     );
   }
@@ -83,6 +94,8 @@ class Gallery extends React.Component {
 Gallery.propTypes = {
   image: PropTypes.any,
   id: PropTypes.any,
+  map: PropTypes.func,
+  gallery: PropTypes.array,
 };
 
 export default Gallery;
