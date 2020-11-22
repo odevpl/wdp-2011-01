4996e701b973fcd2c134d5d07400910ec287550d
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-22 23:31:43 +0100

    Put ColorFilter in ProductList component

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
+import ColorFilter from '../../features/ColorFilter/ColorFilter';
 
 const ProductList = () => (
   <div>
     <div className={styles.root}>This is ProductList</div>
+    <ColorFilter></ColorFilter>
   </div>
 );
-
 
 // ProductList.propTypes = {};
 
