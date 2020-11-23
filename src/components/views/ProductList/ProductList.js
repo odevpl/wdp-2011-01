aa216a5deaff3414ea8466b896abb6f92f204d56
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-11-23 19:41:53 +0100

    Changes saved

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,12 +2,10 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import TagFilter from '../../features/TagFilter/TagFilter';
-import ColorFilter from '../../features/ColorFilter/ColorFilter';
 
 const ProductList = () => (
   <div>
     <div className={styles.root}>This is ProductList</div>
-    <ColorFilter />
     <TagFilter />
   </div>
 );
