51e5010c13a47cf37826b1ed317164180e5a42cb
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-12-15 00:07:27 +0100

    Fix sale component - add onClick method

--- src/components/common/Sale/Sale.js
+++ src/components/common/Sale/Sale.js
@@ -4,14 +4,24 @@ import styles from './Sale.module.scss';
 import { FaRegWindowClose } from 'react-icons/fa';
 import { useHistory } from 'react-router-dom';
 
-const Sale = ({ saleContent }) => {
+const Sale = ({ saleContent, saleFeedback }) => {
   const [sale, setSale] = useState(false);
-  const random = saleContent[Math.floor(Math.random() * saleContent.length)];
+  const [feedback, setFeedback] = useState(false);
+  const [timer, setTimer] = useState(5);
+  const randomSale = saleContent[Math.floor(Math.random() * saleContent.length)];
   const ref = useRef(null);
+  const close = useRef(null);
   const history = useHistory();
 
   history.listen(() => {
-    setSale(true);
+    let time;
+    const rand = Math.floor(Math.random() * 3) + 1;
+    if (rand === 1) {
+      time = setTimeout(() => {
+        setSale(true);
+      }, 2500);
+    }
+    return () => clearTimeout(time);
   });
 
   const closeSale = () => {
@@ -31,8 +41,27 @@ const Sale = ({ saleContent }) => {
   };
 
   const moveToProduct = e => {
-    console.log('advert');
+    if (ref.current.contains(e.target) && !close.current.contains(e.target)) {
+      setSale(false);
+      setFeedback(true);
+    }
   };
+
+  useEffect(() => {
+    let interval = null;
+    if (feedback) {
+      setTimeout(() => {
+        setFeedback(false);
+        setTimer(5);
+      }, 5000);
+      interval = setInterval(() => {
+        setTimer(timer => timer - 1);
+      }, 1000);
+    } else if (!feedback && timer === 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [feedback, timer]);
 
   useEffect(() => {
     document.addEventListener('keydown', closeSaleKey);
@@ -43,24 +72,43 @@ const Sale = ({ saleContent }) => {
     };
   });
 
-  return (
-    sale && (
-      <div key={random.id} className={styles.root}>
-        <div ref={ref} className={styles.main} onClick={moveToProduct}>
-          <div key={random.id} className={styles.saleContainer}>
-            <img src={random.img} alt='' />
-            <div className={styles.saleContent}>
-              <h1>{random.title}</h1>
-              <h2>{random.subtitle}</h2>
-              <h4>{random.promo}</h4>
-              <div className={styles.reduce}>
-                <span>{random.reduce}</span>
-              </div>
+  return sale ? (
+    <div key={randomSale.id} className={styles.root}>
+      <div ref={ref} className={styles.main} onClick={moveToProduct}>
+        <div key={randomSale.id} className={styles.saleContainer}>
+          <img src={randomSale.img} alt='' />
+          <div className={styles.saleContent}>
+            <h1>{randomSale.title}</h1>
+            <h2>{randomSale.subtitle}</h2>
+            <h4>{randomSale.promo}</h4>
+            <div className={styles.reduce}>
+              <span>{randomSale.reduce}</span>
             </div>
-            <div className={styles.close}>
-              <button onClick={closeSale}>
-                <FaRegWindowClose className={styles.icon} />
-              </button>
+          </div>
+          <div ref={close} className={styles.close}>
+            <button onClick={closeSale}>
+              <FaRegWindowClose className={styles.icon} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    feedback && (
+      <div key={saleFeedback.id} className={styles.root}>
+        <div className={styles.main}>
+          <div className={styles.saleContainer}>
+            <img src={saleFeedback.img} className={styles.imgFeedback} alt='' />
+            <div className={styles.saleContent}>
+              <h1>{saleFeedback.title}</h1>
+              <h2>{saleFeedback.subtitle}</h2>
+              <h4>
+                {saleFeedback.promo} in
+                <span className={styles.timer}>{timer}...</span>
+              </h4>
+              <div className={styles.reduce}>
+                <span>{saleFeedback.reduce}</span>
+              </div>
             </div>
           </div>
         </div>
@@ -70,7 +118,8 @@ const Sale = ({ saleContent }) => {
 };
 
 Sale.propTypes = {
-  saleContent: PropTypes.any,
+  saleContent: PropTypes.array,
+  saleFeedback: PropTypes.object,
 };
 
 export default Sale;
