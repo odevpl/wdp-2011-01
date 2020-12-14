916260915de7815898824b7f5c437b8eebaca6a2
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-12-14 14:04:47 +0100

    Add arrow navigation to searchBar list

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -11,20 +11,24 @@ const ProductSearch = ({ products }) => {
   const [display, setDisplay] = useState(false);
   const [displayFilter, setDisplayFilter] = useState(false);
   const [searchProduct, setSearchProduct] = useState([]);
+  const [productCounter, setProductCounter] = useState(-1);
+  const searchList = useRef(null);
+  const searchItem = useRef(null);
 
-  const ref = useRef(null);
-
-  const showDisplay = () => {
+  const showDisplay = e => {
     setDisplay(!display);
+    moveUpAndDownOptions(e);
   };
 
   const showDisplayKey = e => {
-    if (e.target.value.length >= 3) {
-      setDisplayFilter(true);
-      setDisplay(false);
-    } else if (e.target.value.length < 3) {
-      setDisplayFilter(false);
-      setDisplay(false);
+    if (e.keyCode !== 40 && e.keyCode !== 38) {
+      if (e.target.value.length >= 3) {
+        setDisplayFilter(true);
+        setDisplay(false);
+      } else if (e.target.value.length < 3) {
+        setDisplayFilter(false);
+        setDisplay(false);
+      }
     }
 
     if (e.target.value.includes(' ')) {
@@ -41,6 +45,32 @@ const ProductSearch = ({ products }) => {
       );
       setSearchProduct(filteredProducts);
     }
+    moveUpAndDownOptions(e);
+  };
+
+  const moveUpAndDownOptions = e => {
+    setProductCounter(-1);
+    switch (e.keyCode) {
+      case 38:
+        setProductCounter(productCounter - 1);
+        if (productCounter === 0) {
+          setProductCounter(searchProduct.length);
+        }
+        break;
+      case 40:
+        setProductCounter(productCounter + 1);
+        if (productCounter === searchProduct.length) {
+          setProductCounter(0);
+        }
+        // console.log(e.target);
+        // console.log(document.activeElement);
+        // console.log(searchList.current);
+        if (e.target === searchList.current) {
+          e.target.focus();
+          console.log('wow');
+        }
+        break;
+    }
   };
 
   const closeSearchKey = e => {
@@ -51,18 +81,27 @@ const ProductSearch = ({ products }) => {
   };
 
   const closeSearchOutside = e => {
-    if (ref.current && !ref.current.contains(e.target)) {
+    if (searchList.current && !searchList.current.contains(e.target)) {
       setDisplay(false);
       setDisplayFilter(false);
+    }
+  };
+
+  const onMouseOver = e => {
+    if (searchItem.current && searchItem.current.contains(e.target)) {
+      setProductCounter(-1);
     }
   };
 
   useEffect(() => {
     document.addEventListener('keydown', closeSearchKey);
     document.addEventListener('click', closeSearchOutside);
+    document.addEventListener('mouseover', onMouseOver);
+
     return () => {
       document.removeEventListener('keydown', closeSearchKey);
       document.removeEventListener('click', closeSearchOutside);
+      document.removeEventListener('mouseover', onMouseOver);
     };
   });
 
@@ -72,20 +111,25 @@ const ProductSearch = ({ products }) => {
         <div className={styles.category}>
           <div className={styles.searchField}>
             <input
-              ref={ref}
+              ref={searchList}
               placeholder='Search products...'
               type='text'
-              onFocus={showDisplay}
+              onFocus={e => showDisplay(e)}
               onKeyUp={e => showDisplayKey(e)}
             />
             <Button className={styles.searchButton}>
               <FontAwesomeIcon className={styles.icon} icon={faSearch} />
             </Button>
             {display && (
-              <div className={styles.autoContainer}>
+              <div className={styles.autoContainer} ref={searchItem}>
                 {products.map((option, id) => {
                   return (
-                    <div key={id} className={styles.option}>
+                    <div
+                      key={id}
+                      className={`${styles.option} ${
+                        id === productCounter ? styles.test : ''
+                      }`}
+                    >
                       <Link
                         className={styles.link}
                         to={__dirname + `product/${option.id}`}
@@ -101,9 +145,14 @@ const ProductSearch = ({ products }) => {
               <div className={styles.autoContainer}>
                 {searchProduct.map((product, id) => {
                   return (
-                    <div key={id} className={styles.option}>
+                    <div
+                      key={id}
+                      className={`${styles.option} ${
+                        id === productCounter ? styles.test : ''
+                      }`}
+                    >
                       <Link
-                        className={styles.link}
+                        className={`${styles.link} ${styles.testLink}`}
                         to={__dirname + `product/${product.id}`}
                       >
                         {product.name}
